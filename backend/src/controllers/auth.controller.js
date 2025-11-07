af3cf8b9d8394db6c971396f3328859b10bb497a
Neha07277 <neha.pal072002@gmail.com>
2025-11-07 16:00:13 +0530

    update profile added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,8 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { ENV } from "../lib/env.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import cloudinary from "../lib/cloudinary.js";
+//import bcrypt from "bcryptjs";
 
 export const  signup =  async (req,res) => {
     const {fullName, email, password} = req.body
@@ -102,24 +104,24 @@ export const logout = (_, res) => {
 
 export const updateProfile = async (req, res) => {
   try {
-    const { profilePic } = req.body;
-    if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
+   const { profilePic } = req.body;
+   if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
 
-    const userId = req.user._id;
+     const userId = req.user._id;
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
 
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
+     const updatedUser = await User.findByIdAndUpdate(
+       userId,
       { profilePic: uploadResponse.secure_url },
       { new: true }
-    );
+     );
 
     res.status(200).json(updatedUser);
-  } catch (error) {
-    console.log("Error in update profile:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+   } catch (error) {
+     console.log("Error in update profile:", error);
+     res.status(500).json({ message: "Internal server error" });
+   }
 };
 
 
