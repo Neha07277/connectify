51202fb1dce6d97791e8a02bf6da08c7af80b2da
Neha07277 <neha.pal072002@gmail.com>
2025-11-23 21:27:35 +0530

    CORS fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
 const __dirname = path.resolve();
-const PORT = process.env.PORT || ENV.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 // --------------------
 // MIDDLEWARE (should come BEFORE routes)
@@ -20,7 +20,16 @@ const PORT = process.env.PORT || ENV.PORT || 3000;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://connectify-h1y5t.sevalla.app"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 
 // --------------------
 // API ROUTES (After middleware only)
